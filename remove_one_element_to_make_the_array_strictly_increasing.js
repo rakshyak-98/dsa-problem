3a6efd6d25650c65ff633cfe02badf8cec70fdb1
rakshyak-98 <rakshak1998@gmail.com>
2026-04-01 01:58:53 +0530

    update logic

--- remove_one_element_to_make_the_array_strictly_increasing.js
+++ remove_one_element_to_make_the_array_strictly_increasing.js
@@ -38,24 +38,19 @@
  */
 var canBeIncreasing = function (nums) {
   let count = 0;
-
+  let prev = nums[0];
   for (let i = 1; i < nums.length; i++) {
-    if (nums[i] <= nums[i - 1]) {
+    if (nums[i] <= prev) {
       count++;
-
       if (count > 1) return false;
-
-      if (
-        i > 1 &&
-        nums[i] <= nums[i - 2] &&
-        i + 1 < nums.length &&
-        nums[i + 1] <= nums[i - 1]
-      ) {
-        return false;
+      if (i > 1 && nums[i] <= nums[i - 2]) {
+        continue;
       }
+      prev = nums[i];
+    } else {
+      prev = nums[i];
     }
   }
-
   return true;
 };
 console.log(canBeIncreasing([1, 2, 10, 5, 7])); // true
