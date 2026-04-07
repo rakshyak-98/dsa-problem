3bf9bc1d39d53af5d025169114adbf0fdc05a586
rakshyak-98 <rakshak1998@gmail.com>
2026-04-07 19:29:03 +0530

    add solution reference

--- can_place_flower.js
+++ can_place_flower.js
@@ -42,5 +42,5 @@ var canPlaceFlowers = function (flowerbed, n) {
   return n <= 0;
 };
 
-console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1));
-console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2));
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1)); // true
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2)); // false
