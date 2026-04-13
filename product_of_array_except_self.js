67fec8fc7bb004fdf981b435edaabd691d3a9635
Raksyak-MST <rakshyak@graspberry.com>
2026-04-13 14:31:39 +0530

    add solution

--- product_of_array_except_self.js
+++ product_of_array_except_self.js
@@ -26,4 +26,21 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function (nums) {};
+var productExceptSelf = function (nums) {
+  const res = [];
+  res[0] = 1;
+  for (let i = 1; i < nums.length; i++) {
+    res[i] = res[i - 1] * nums[i - 1];
+  }
+
+  let rightProduct = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    res[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+
+  return res;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4]));
+console.log(productExceptSelf([-1, 1, 0, -3, 3]));
