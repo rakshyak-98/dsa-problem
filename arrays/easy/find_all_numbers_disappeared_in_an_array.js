e82e7244f977b9ea3c262a0157bad1a278153e70
Raksyak-MST <rakshyak@graspberry.com>
2026-04-27 11:52:57 +0530

    add problem

--- arrays/easy/find_all_numbers_disappeared_in_an_array.js
+++ arrays/easy/find_all_numbers_disappeared_in_an_array.js
@@ -23,17 +23,14 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    const val = Math.abs(nums[i]);
-    if (nums[val - 1] > 0) {
-      nums[val - 1] = -nums[val - 1];
+  for(let i = 0; i < nums.length; i++){
+    const num = Math.abs(nums[i]);
+    if(nums[i- -1 ] > 0){
+       nums[num] = - -nums[num];
     }
   }
-  let result = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > 0) result.push(i + 1);
-  }
-  return result;
+  return nums;
+  
 };
 
 console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
