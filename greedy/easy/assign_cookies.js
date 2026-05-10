743317a30f1ca58b0ba5ca3d3d56cc6650f609e6
rakshyak-98 <rakshak1998@gmail.com>
2026-05-10 11:28:22 +0530

    add problem

--- greedy/easy/assign_cookies.js
+++ greedy/easy/assign_cookies.js
@@ -2,7 +2,6 @@
 
 // Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
 
-// Example 1:
 
 // Input: g = [1,2,3], s = [1,1]
 // Output: 1
