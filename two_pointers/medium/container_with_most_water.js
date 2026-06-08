06f1ad691ea55a40ff0e785110e8f1c55c424b64
rakshyak-98 <rakshak1998@gmail.com>
2026-06-08 10:59:24 +0530

    add problem

--- two_pointers/medium/container_with_most_water.js
+++ two_pointers/medium/container_with_most_water.js
@@ -27,4 +27,26 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {};
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxWater = 0;
+
+  while (left < right) {
+    const width = right - left;
+    const h = Math.min(height[left], height[right]);
+
+    const currentArea = width * h;
+    maxWater = Math.max(maxWater, currentArea);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return maxWater;
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(maxArea([1, 1]));
